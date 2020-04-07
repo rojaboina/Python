2ee57e7cecb0fd04d66ecfa3b748258543f23f96
Roja boina <rojaboina@Rojas-MacBook.local>
2020-04-07 10:55:59 -0400

    chapter2

---
+++ code/name_case.py
@@ -0,0 +1,21 @@
+Eric="Hello eric would you like to learn some python today?"
+print(Eric);
+
+a="Roja"
+print(a.lower());
+print(a.upper());
+print(a.title());
+
+famous_person=' "A person \n who never made a mistake never tried \n anything new " '
+message = f"Albert Einstein once said,{famous_person}"
+print(message);
+
+famous_person=" A person \n who never made a mistake never tried \n anything new \" "
+message ="Albert Einstein once said"
+a= f"{message}, {famous_person} ";
+
+person="\troja\n"
+print(person);
+print(person.lstrip());
+print(person.rstrip());
+print(person.strip());
